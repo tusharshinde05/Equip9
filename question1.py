3b51c0c5a99e72758a480c57b94dfbbc01e65440
tusharshinde05 <tusharjsshinde@gmail.com>
2025-04-01 13:14:16 +0530

    Update and rename question1 to question1.py

--- question1.py
+++ question1.py
@@ -5,9 +5,14 @@ def find_shortest_path(n, edges, availability, start_provider, target_equipment)
     for a, b in edges:
         graph[a].append(b)
         graph[b].append(a)
+
+    for node in graph:
+        graph[node].sort(reverse=True)
     
     queue = deque([(start_provider, [start_provider])])
-    visited = set([start_provider])  
+    visited = set([start_provider])
+
+    possible_paths = []
 
     while queue:
         current, path = queue.popleft()
@@ -17,10 +22,10 @@ def find_shortest_path(n, edges, availability, start_provider, target_equipment)
                 visited.add(neighbor)
                 new_path = path + [neighbor]
                 if target_equipment in availability.get(neighbor, []):
-                    return new_path
+                    possible_paths.append(new_path)
                 queue.append((neighbor, new_path))
     
-    return -1
+    return max(possible_paths, key = lambda p: p[-1]) if possible_paths else -1
 
 n = 5
 edges = [(1, 2), (2, 3), (3, 4), (4, 5)]
